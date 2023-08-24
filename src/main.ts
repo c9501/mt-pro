e222001d910a273ae78d4bc090b0c974972d43f0
c9501 <549105847@qq.com>
2023-08-24 11:43:00 +0800

    feat(global): pinia,vant

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+import pinia from './stores'
 import App from './App.vue'
 import router from './router'
-
+import 'vant/lib/index.css'
+import './styles/main.scss'
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
-
+app.use(pinia)
 app.mount('#app')
